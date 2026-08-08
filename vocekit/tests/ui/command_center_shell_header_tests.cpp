25fd5eda1cacbcffcaa99d7130caa437ee6c45b3
tomhuaijbig <tomhuaijbig@users.noreply.github.com>
2026-08-08 09:12:35 +0800

    test: require add function inside sidebar list

--- vocekit/tests/ui/command_center_shell_header_tests.cpp
+++ vocekit/tests/ui/command_center_shell_header_tests.cpp
@@ -2,7 +2,13 @@
 
 #include "../../src/ui/command_center_shell.h"
 
+#include <QCoreApplication>
+#include <QEvent>
 #include <QFile>
+#include <QPushButton>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QVBoxLayout>
 #include <type_traits>
 
 class CommandCenterShellHeaderTests : public QObject
@@ -12,6 +18,7 @@ class CommandCenterShellHeaderTests : public QObject
 private slots:
     void exposesNavigationShellInterface();
     void hubWindowDoesNotRenderNavigationControls();
+    void addFunctionActionFollowsFunctionRows();
 };
 
 void CommandCenterShellHeaderTests::exposesNavigationShellInterface()
@@ -44,6 +51,105 @@ void CommandCenterShellHeaderTests::hubWindowDoesNotRenderNavigationControls()
     QVERIFY(!contents.contains("m_commandSearchEdit"));
 }
 
-QTEST_APPLESS_MAIN(CommandCenterShellHeaderTests)
+void CommandCenterShellHeaderTests::addFunctionActionFollowsFunctionRows()
+{
+    QVector<CommandCenterFunctionItem> functions;
+    CommandCenterFunctionItem dictate;
+    dictate.id = QStringLiteral("dictate");
+    dictate.title = QString::fromUtf8("听写");
+    functions.append(dictate);
+
+    CommandCenterFunctionItem custom;
+    custom.id = QStringLiteral("custom_1");
+    custom.title = QString::fromUtf8("自定义功能 1");
+    functions.append(custom);
+
+    int addFunctionCalls = 0;
+    CommandCenterShellAccess access;
+    access.functionsProvider = [&functions]() {
+        return functions;
+    };
+    access.addFunction = [&addFunctionCalls]() {
+        ++addFunctionCalls;
+    };
+
+    CommandCenterShell shell(access, new QWidget);
+    shell.resize(720, 220);
+    shell.show();
+    QCoreApplication::processEvents();
+
+    const QString addFunctionText = QString::fromUtf8("新增功能");
+    const auto addButtonsByText = [&shell, &addFunctionText]() {
+        QList<QPushButton *> matches;
+        const QList<QPushButton *> buttons = shell.findChildren<QPushButton *>();
+        for (QPushButton *button : buttons) {
+            if (button->text() == addFunctionText) {
+                matches.append(button);
+            }
+        }
+        return matches;
+    };
+
+    auto *scroll = shell.findChild<QScrollArea *>(QStringLiteral("commandFunctionScroll"));
+    auto *add = shell.findChild<QPushButton *>(QStringLiteral("commandAddFunctionButton"));
+    QVERIFY(scroll);
+    QVERIFY(add);
+    const QList<QPushButton *> initialTextMatches = addButtonsByText();
+    QCOMPARE(initialTextMatches.size(), 1);
+    QCOMPARE(initialTextMatches.first(), add);
+    QVERIFY(scroll->widget());
+    QVERIFY(scroll->widget()->isAncestorOf(add));
+
+    auto *functionLayout = qobject_cast<QVBoxLayout *>(scroll->widget()->layout());
+    QVERIFY(functionLayout);
+    QCOMPARE(functionLayout->count(), functions.size() + 2);
+    auto *dictateButton = qobject_cast<QPushButton *>(functionLayout->itemAt(0)->widget());
+    auto *customButton = qobject_cast<QPushButton *>(functionLayout->itemAt(1)->widget());
+    QVERIFY(dictateButton);
+    QVERIFY(customButton);
+    QCOMPARE(dictateButton->property("functionId").toString(), QStringLiteral("dictate"));
+    QCOMPARE(customButton->property("functionId").toString(), QStringLiteral("custom_1"));
+    QCOMPARE(functionLayout->itemAt(functions.size())->widget(), add);
+    QVERIFY(functionLayout->itemAt(functions.size() + 1)->spacerItem());
+
+    scroll->ensureWidgetVisible(add);
+    QCoreApplication::processEvents();
+    QTest::mouseClick(add, Qt::LeftButton);
+    QCOMPARE(addFunctionCalls, 1);
+
+    shell.refreshFunctions();
+    QCoreApplication::sendPostedEvents(nullptr, QEvent::DeferredDelete);
+    QCoreApplication::processEvents();
+
+    const QList<QPushButton *> refreshedTextMatches = addButtonsByText();
+    QCOMPARE(refreshedTextMatches.size(), 1);
+    const QList<QPushButton *> addButtons =
+        shell.findChildren<QPushButton *>(QStringLiteral("commandAddFunctionButton"));
+    QCOMPARE(addButtons.size(), 1);
+    add = addButtons.first();
+    QCOMPARE(refreshedTextMatches.first(), add);
+    QVERIFY(scroll->widget()->isAncestorOf(add));
+    functionLayout = qobject_cast<QVBoxLayout *>(scroll->widget()->layout());
+    QVERIFY(functionLayout);
+    QCOMPARE(functionLayout->count(), functions.size() + 2);
+    dictateButton = qobject_cast<QPushButton *>(functionLayout->itemAt(0)->widget());
+    customButton = qobject_cast<QPushButton *>(functionLayout->itemAt(1)->widget());
+    QVERIFY(dictateButton);
+    QVERIFY(customButton);
+    QCOMPARE(dictateButton->property("functionId").toString(), QStringLiteral("dictate"));
+    QCOMPARE(customButton->property("functionId").toString(), QStringLiteral("custom_1"));
+    QCOMPARE(functionLayout->itemAt(functions.size())->widget(), add);
+    QVERIFY(functionLayout->itemAt(functions.size() + 1)->spacerItem());
+
+    QVERIFY(scroll->verticalScrollBar()->maximum() > 0);
+    scroll->verticalScrollBar()->setValue(scroll->verticalScrollBar()->maximum());
+    QCoreApplication::processEvents();
+    const QRect addRect(add->mapTo(scroll->viewport(), QPoint(0, 0)), add->size());
+    QVERIFY(scroll->viewport()->rect().intersects(addRect));
+    QTest::mouseClick(add, Qt::LeftButton);
+    QCOMPARE(addFunctionCalls, 2);
+}
+
+QTEST_MAIN(CommandCenterShellHeaderTests)
 
 #include "command_center_shell_header_tests.moc"
--- vocekit/tests/ui/command_center_shell_header_tests.pro
+++ vocekit/tests/ui/command_center_shell_header_tests.pro
@@ -6,7 +6,11 @@ CONFIG -= app_bundle
 TEMPLATE = app
 TARGET = command_center_shell_header_tests
 
-SOURCES += command_center_shell_header_tests.cpp
+SOURCES += \
+    command_center_shell_header_tests.cpp \
+    ../../src/ui/command_center_shell.cpp \
+    ../../src/ui/command_search_router.cpp
 
 HEADERS += \
-    ../../src/ui/command_center_shell.h
+    ../../src/ui/command_center_shell.h \
+    ../../src/ui/command_search_router.h
